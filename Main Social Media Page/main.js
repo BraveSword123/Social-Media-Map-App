7f9765b148ae7c40c5ee01dfb019d1c87c5424df
annabelle.gyeniba.moore@gmail.com <annabelle.gyeniba.moore@gmail.com>
2026-01-17 09:44:04 -0800

    Enhance UI interactions with hover and active effects for posts and badges; add create post button functionality

--- Main Social Media Page/main.js
+++ Main Social Media Page/main.js
@@ -1,10 +1,27 @@
 function showPosts(){
     document.getElementById("postContainer").style.display = 'flex'; 
-    document.getElementById("badgeContainer").style.display = 'none'; 
+    document.getElementById("profileButton").style.backgroundColor = 'rgb(255, 105, 160)'; 
+    document.getElementById("badgesButton").style.backgroundColor = 'rgb(255, 105, 160)'; 
+    document.getElementById("postsButton").style.backgroundColor = 'rgb(251, 169, 199)';  
+    document.getElementById("badgeContainer").style.display = 'none'
+    document.getElementById("profileContainer").style.display = 'none'; ; 
     // Hide all of the other containers 
 }
 
 function showBadges(){
     document.getElementById("badgeContainer").style.display = 'block'; 
+    document.getElementById("profileButton").style.backgroundColor = 'rgb(255, 105, 160)'; 
+    document.getElementById("badgesButton").style.backgroundColor = 'rgb(251, 169, 199)'; 
+    document.getElementById("postsButton").style.backgroundColor = 'rgb(255, 105, 160)'; 
+    document.getElementById("postContainer").style.display = 'none';
+    document.getElementById("profileContainer").style.display = 'none';  
+}
+
+function showProfile(){
+    document.getElementById("profileContainer").style.display = 'block';
+    document.getElementById("profileButton").style.backgroundColor = 'rgb(251, 169, 199)'; 
+    document.getElementById("badgesButton").style.backgroundColor = 'rgb(255, 105, 160)'; 
+    document.getElementById("postsButton").style.backgroundColor = 'rgb(255, 105, 160)'; 
     document.getElementById("postContainer").style.display = 'none'; 
+    document.getElementById("badgeContainer").style.display = 'none'
 }
