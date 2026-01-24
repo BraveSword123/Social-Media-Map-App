d5161737680881d65bbe950c35378f0e1e8b92c9
annabelle.gyeniba.moore@gmail.com <annabelle.gyeniba.moore@gmail.com>
2026-01-23 21:07:55 -0800

    Fix hover and click functions for navbar elements; implement event listeners for mouseenter and mouseleave

--- Main Social Media Page/main.js
+++ Main Social Media Page/main.js
@@ -1,4 +1,36 @@
 
+// Annabelle Needs to Fix window.onload() function
+/* window.onload = document.getElementsByClassName("navbarElement").onmouseout = function() {
+    document.getElementsByClassName("navbarElement").style.backgroundColor = 'rgb(255, 105, 160)';
+    // document.getElementsByClassName("navbarElement").style.transition = "all 3s";  
+}
+ window.onload = document.getElementsByClassName("navbarElement").onmouseover = function() {
+    document.getElementsByClassName("navbarElement").style.backgroundColor = 'rgb(251, 169, 199)';
+    // document.getElementsByClassName("navbarElement").style.transition = "all 3s";  
+}   */
+
+
+// Array from() converts HTML Collection to an array
+ /* Array.from(document.getElementsByClassName("navbarElement")).onmouseout.forEach(function () {
+    document.getElementsByClassName("navbarElement").style.backgroundColor = 'rgb(255, 105, 160)';
+ });
+
+
+  Array.from(document.getElementsByClassName("navbarElement")).onmouseover.forEach(function () {
+    document.getElementsByClassName("navbarElement").style.backgroundColor =  'rgb(251, 169, 199)';
+ }); */
+
+
+ // If the Dom is ready
+ window.onload = document.addEventListener("DOMContentLoaded", () => {
+   document.querySelectorAll(".navbarElement").forEach(elem => {
+        elem.addEventListener("mouseenter", () => elem.style.backgroundColor = 'rgb(251, 169, 199)');
+        elem.addEventListener("mouseleave", () => elem.style.backgroundColor = 'rgb(255, 105, 160)');
+    });
+});
+
+//  document.addEventListener("click", () => elem.style.backgroundColor = 'rgb(251, 169, 199)');
+
 function showPosts(){
     document.getElementById("postContainer").style.display = 'flex'; 
     document.getElementById("profileButton").style.backgroundColor = 'rgb(255, 105, 160)'; 
