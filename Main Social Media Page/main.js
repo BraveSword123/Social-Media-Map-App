cd206a1e2dffa3744b618807c8707d362194b44d
annabelle.gyeniba.moore@gmail.com <annabelle.gyeniba.moore@gmail.com>
2026-01-24 21:05:59 -0800

    Refactor navbar element styles and functionality; implement active state management and clean up event listeners

--- Main Social Media Page/main.js
+++ Main Social Media Page/main.js
@@ -1,42 +1,23 @@
 
-// Annabelle Needs to Fix window.onload() function
-/* window.onload = document.getElementsByClassName("navbarElement").onmouseout = function() {
-    document.getElementsByClassName("navbarElement").style.backgroundColor = 'rgb(255, 105, 160)';
-    // document.getElementsByClassName("navbarElement").style.transition = "all 3s";  
-}
- window.onload = document.getElementsByClassName("navbarElement").onmouseover = function() {
-    document.getElementsByClassName("navbarElement").style.backgroundColor = 'rgb(251, 169, 199)';
-    // document.getElementsByClassName("navbarElement").style.transition = "all 3s";  
-}   */
-
-
-// Array from() converts HTML Collection to an array
- /* Array.from(document.getElementsByClassName("navbarElement")).onmouseout.forEach(function () {
-    document.getElementsByClassName("navbarElement").style.backgroundColor = 'rgb(255, 105, 160)';
- });
-
-
-  Array.from(document.getElementsByClassName("navbarElement")).onmouseover.forEach(function () {
-    document.getElementsByClassName("navbarElement").style.backgroundColor =  'rgb(251, 169, 199)';
- }); */
-
-
- // If the Dom is ready
- window.onload = document.addEventListener("DOMContentLoaded", () => {
-   document.querySelectorAll(".navbarElement").forEach(elem => {
-        elem.addEventListener("mouseenter", () => elem.style.backgroundColor = 'rgb(251, 169, 199)');
-        elem.addEventListener("mouseleave", () => elem.style.backgroundColor = 'rgb(255, 105, 160)');
-    });
-});
-
-//  document.addEventListener("click", () => elem.style.backgroundColor = 'rgb(251, 169, 199)');
+// Once the HTML is loaded and built in the page run this code 
+document.addEventListener("DOMContentLoaded", () => {
+// A variable that holds the container of all the nav bar buttons 
+const group = document.querySelector("#navbarContainer"); 
+// A container of all buttons in nav bar 
+const buttons = group.querySelectorAll(".navbarElement"); 
+// Loop through each button that has class navbarElement
+buttons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        // Remove active from all buttons
+        buttons.forEach(b => b.classList.remove("active")); 
+        // Add active to the button that we clicked 
+        btn.classList.add("active"); 
+        }); 
+    }); 
+}); 
 
 function showPosts(){
     document.getElementById("postContainer").style.display = 'flex'; 
-    document.getElementById("profileButton").style.backgroundColor = 'rgb(255, 105, 160)'; 
-    document.getElementById("badgesButton").style.backgroundColor = 'rgb(255, 105, 160)'; 
-    document.getElementById("postsButton").style.backgroundColor = 'rgb(251, 169, 199)'; 
-    document.getElementById("locationsButton").style.backgroundColor = 'rgb(255, 105, 160)'; 
     document.getElementById("badgeContainer").style.display = 'none'; 
     document.getElementById("profileContainer").style.display = 'none'; 
     document.getElementById("locationsContainer").style.display = 'none'; 
@@ -44,21 +25,13 @@ function showPosts(){
 
 function showBadges(){
     document.getElementById("badgeContainer").style.display = 'block'; 
-    document.getElementById("profileButton").style.backgroundColor = 'rgb(255, 105, 160)'; 
-    document.getElementById("badgesButton").style.backgroundColor = 'rgb(251, 169, 199)'; 
-    document.getElementById("postsButton").style.backgroundColor = 'rgb(255, 105, 160)'; 
-    document.getElementById("locationsButton").style.backgroundColor = 'rgb(255, 105, 160)'; 
     document.getElementById("postContainer").style.display = 'none';
     document.getElementById("profileContainer").style.display = 'none';  
     document.getElementById("locationsContainer").style.display = 'none';
 }
 
 function showProfile(){
-    document.getElementById("profileContainer").style.display = 'block';
-    document.getElementById("profileButton").style.backgroundColor = 'rgb(251, 169, 199)'; 
-    document.getElementById("badgesButton").style.backgroundColor = 'rgb(255, 105, 160)'; 
-    document.getElementById("postsButton").style.backgroundColor = 'rgb(255, 105, 160)';
-    document.getElementById("locationsButton").style.backgroundColor = 'rgb(255, 105, 160)';  
+    document.getElementById("profileContainer").style.display = 'block'; 
     document.getElementById("postContainer").style.display = 'none'; 
     document.getElementById("badgeContainer").style.display = 'none'
     document.getElementById("locationsContainer").style.display = 'none';
@@ -66,10 +39,6 @@ function showProfile(){
 
 function showLocations(){
     document.getElementById("locationsContainer").style.display = 'block'; 
-    document.getElementById("locationsButton").style.backgroundColor = 'rgb(251, 169, 199)';
-    document.getElementById("badgesButton").style.backgroundColor = 'rgb(255, 105, 160)'; 
-    document.getElementById("postsButton").style.backgroundColor = 'rgb(255, 105, 160)';
-    document.getElementById("profileButton").style.backgroundColor = 'rgb(255, 105, 160)';
     document.getElementById("badgeContainer").style.display = 'none'; 
     document.getElementById("profileContainer").style.display = 'none'; 
     document.getElementById("postContainer").style.display = 'none'; 
